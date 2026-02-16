77b7f7ac4344e2af87f80256d99af3b58d5fab24
Горохов Кирилл <GKA@planeta.stend>
2026-02-16 11:52:43 +0300

    изменение файла logic.py

--- logic.py
+++ logic.py
@@ -1,17 +1,33 @@
 import secrets
 import time 
 
-num_one = secrets.choice(range(1,9))
-num_two = secrets.choice(range(1,9))
-num_three = secrets.choice(range(1,9))
 
-list = [num_one, num_two, num_three]
-newstr = ""
+def spin_reels():
+    num_one = secrets.choice(range(1,9))
+    num_two = secrets.choice(range(1,9))
+    num_three = secrets.choice(range(1,9))
 
-for i in list:
-    string = str(i)
-    newstr += string
-    print(f'\r{newstr}', end='', flush=True)
-    time.sleep(1)
+    result = [num_one, num_two, num_three]
 
-print()
+    if num_one == num_two == num_three:
+        message = "ДЖЕКПОТ! ВСЕ ТРИ!"
+    else:
+        message = "Попробуй еще раз!"
+        
+    return {
+            "reels": result,
+            "message": message,
+            "win": num_one == num_two == num_three
+        }
+
+if __name__ == "__main__":
+    result = spin_reels()
+    numbers = result["reels"]
+    newstr = ""
+    
+    print("Пошла крутка:")
+    for i in numbers:
+        newstr += str(i)
+        print(f'\r{newstr}', end='', flush=True)
+        time.sleep(1)
+    print(f"\n{result['message']}")
