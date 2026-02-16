74742643e67b3622ec8919ce2377a6992c0feba0
Горохов Кирилл <GKA@planeta.stend>
2026-02-16 11:41:37 +0300

    rename file and change some shit

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import secrets
-import time 
+from fastapi import FastAPI, Response, Request, Path, Query, Body
+from fastapi.responses import HTMLResponse, PlainTextResponse, FileResponse, JSONResponse, RedirectResponse
+import uvicorn
+from fastapi.staticfiles import StaticFiles
 
-num_one = secrets.choice(range(1,9))
-num_two = secrets.choice(range(1,9))
-num_three = secrets.choice(range(1,9))
 
-list = [num_one, num_two, num_three]
-newstr = ""
+def read_html_file(file_name):
+    encodings = ['utf-8', 'cp1251', 'iso-8859-1', 'windows-1252']       
+    for encoding in encodings:
+        with open(file_name, "r", encoding=encoding) as file:
+            return file.read()
 
-for i in list:
-    string = str(i)
-    newstr += string
-    print(f'\r{newstr}', end='', flush=True)
-    time.sleep(0.5)
+app = FastAPI()
 
-print()
+@app.get("/")
+def root():
+    htmldata = read_html_file("frontend\\index.html")
+    return HTMLResponse(htmldata)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
